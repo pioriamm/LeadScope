db7446b43e78cfbee5317c975767c195f556df7c
marcelo <pioriam@gmail.com>
2026-02-12 08:28:01 -0300

    criacao do projeto

---
+++ src/main/java/br/com/pioriam/leadscope/LeadScopeApplication.java
@@ -0,0 +1,13 @@
+package br.com.pioriam.leadscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeadScopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pioriam/leadscope/LeadScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pioriam.leadscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadScopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
